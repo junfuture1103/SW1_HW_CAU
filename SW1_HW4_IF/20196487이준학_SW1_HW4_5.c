90b8933145213d18cee1740efb225084c94cccec
JUNTHEWORLD <junhak1103@naver.com>
2021-10-06 09:37:43 +0900

    Modify 4-5 only use if

--- SW1_HW4_IF/20196487이준학_SW1_HW4_5.c
+++ SW1_HW4_IF/20196487이준학_SW1_HW4_5.c
@@ -13,31 +13,71 @@ int main() {
 	tmp = time * 60 + min;
 
 	if (tmp <= 480) {
-		printf("8:00 a.m.   9:16 a.m.");
+		if (1439 - 1305 + tmp < 480 - tmp) {
+			printf("9:45 p.m.    10:58 p.m.");
+		}
+		else {
+			printf("8:00 a.m.   9:16 a.m.");
+		}
 	}
-	else if (tmp > 480 && tmp < 595) {
-		printf("9:55 a.m.   10:52 a.m.");
+	else if (tmp > 480 && tmp <= 595) {
+		if (595 - tmp > tmp - 480) {
+			printf("8:00 a.m.   9:16 a.m.");
+		}
+		else {
+			printf("9:55 a.m.   10:52 a.m.");
+		}
 	}
-	else if (tmp > 595 && tmp < 619) {
-		printf("12:05 p.m.  1:30 p.m.");
+	else if (tmp > 595 && tmp <= 619) {
+		if (619 - tmp > tmp - 595) {
+			printf("9:55 a.m.   10:52 a.m.");
+		}
+		else {
+			printf("10:19 a.m.  11:31 a.m.");
+		}
 	}
-	else if (tmp > 619 && tmp < 725) {
-		printf("2:00 p.m.    3:08 p.m.");
+	else if (tmp > 619 && tmp <= 725) {
+		if (725 - tmp > tmp - 619) {
+			printf("10:19 a.m.  11:31 a.m.");
+		}
+		else {
+			printf("12:05 p.m.  1:30 p.m.");
+		}
 	}
-	else if (tmp > 725 && tmp < 840) {
-		printf("3:45 p.m.    4:55 p.m.");
+	else if (tmp > 725 && tmp <= 840) {
+		if (840 - tmp > tmp - 725) {
+			printf("12:05 p.m.  1:30 p.m.");
+		}
+		else {
+			printf("2:00 p.m.    3:08 p.m.");
+		}
 	}
-	else if (tmp > 840 && tmp < 945) {
-		printf("7:00 p.m.    8:20 p.m.");
+	else if (tmp > 840 && tmp <= 945) {
+		if (945 - tmp > tmp - 840) {
+			printf("2:00 p.m.    3:08 p.m.");
+		}
+		else {
+			printf("3:45 p.m.    4:55 p.m.");
+		}
 	}
-	else if (tmp > 945 && tmp < 1140) {
-		printf("9:45 p.m.    10:58 p.m.");
+	else if (tmp > 945 && tmp <= 1140) {
+		if (1140 - tmp > tmp - 945) {
+			printf("3:45 p.m.    4:55 p.m.");
+		}
+		else {
+			printf("7:00 p.m.    8:20 p.m.");
+		}
 	}
-	else if (tmp > 1140 && tmp < 1305) {
-		printf("9:55 a.m.   10:52 a.m.");
+	else if (tmp > 1140 && tmp <= 1305) {
+		if (1305 - tmp > tmp - 1140) {
+			printf("7:00 p.m.    8:20 p.m.");
+		}
+		else {
+			printf("9:45 p.m.    10:58 p.m.");
+		}
 	}
 	else {
-		printf("Uhm...");
+		printf("9:45 p.m.    10:58 p.m.");
 	}
 
 	return 0;
