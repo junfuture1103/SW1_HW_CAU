94f019a4a968cbbf748ea4a4e0817abce45bde3c
JUNTHEWORLD <junhak1103@naver.com>
2021-10-13 11:18:06 +0900

    Update 20196487이준학_SW1_HW5_3.c

--- SW1_HW5_FOR/20196487이준학_SW1_HW5_3.c
+++ SW1_HW5_FOR/20196487이준학_SW1_HW5_3.c
@@ -5,6 +5,7 @@ int main() {
 	int count = 0;
 	int tmp_size = 0;
 	int avg_size = 0;
+	int empty_flag = 2;
 	int end_flag = 1;
 
 	while (end_flag) {
@@ -15,29 +16,40 @@ int main() {
 			if (tmp_size == 0) {
 				break;
 			}
+			if (empty_flag) {
+				continue;
+			}
 			count++;
 			avg_size += tmp_size;
+			empty_flag = 1;
 			tmp_size = 0;
 			break;
 		case '\t':
 			if (tmp_size == 0) {
 				break;
 			}
+			if (empty_flag) {
+				continue;
+			}
 			count++;
 			avg_size += tmp_size;
+			empty_flag = 1;
 			tmp_size = 0;
 			break;
 		case '\n':
-			count++;
+			if (empty_flag == 0) {
+				count++;
+			}
 			avg_size += tmp_size;
 			end_flag = 0;
 			break;
 		default:
+			empty_flag = 0;
 			tmp_size++;
 		}
 	}
 
-	printf("´Ü¾îÀÇ °¹¼ö´Â %d ÃÑ ¾ËÆÄºª °¹¼ö´Â %d Æò±Õ ´Ü¾î ±æÀÌ´Â %.2f ÀÔ´Ï´Ù.", count, avg_size, (float)avg_size / count);
+	printf("´Ü¾îÀÇ °¹¼ö´Â %d ÃÑ ¾ËÆÄºª °¹¼ö´Â %d Æò±Õ ´Ü¾î ±æÀÌ´Â %.2f ÀÔ´Ï´Ù.", count, avg_size, (count == 0 ? 0 : (float)avg_size / count));
 
 	return 0;
 }
