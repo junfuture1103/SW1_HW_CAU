a21ea4c9ecd9e7ae1f995fb359e5fee8a881460e
JUNTHEWORLD <junhak1103@naver.com>
2021-11-08 22:07:07 +0900

    Update HW9_2_20196487이준학.c

--- SW1_HW9_POINTER_ARRAY_FUNC/HW9_2_20196487이준학.c
+++ SW1_HW9_POINTER_ARRAY_FUNC/HW9_2_20196487이준학.c
@@ -6,8 +6,8 @@
 #define ENG 1
 #define MATH 2
 
-void sort(int list[], char name[], char major[], int index) {
-	int min_index, temp;
+void sort(int list[], int stu_num[], char name[], char major[], int index) {
+	int min_index, temp, temp2;
 	char name_tmp, major_tmp;
 
 	for (int i = 0; i < index - 1; i++) {
@@ -17,6 +17,10 @@ void sort(int list[], char name[], char major[], int index) {
 				min_index = j;
 			}
 		}
+		temp2 = stu_num[i];
+		stu_num[i] = stu_num[min_index];
+		stu_num[min_index] = temp2;
+
 		temp = list[i];
 		list[i] = list[min_index];
 		list[min_index] = temp;
@@ -34,6 +38,7 @@ void sort(int list[], char name[], char major[], int index) {
 
 int main() {
 	int score[SUB_NUM][STU_NUM] = { 0, }; // SUB0 : KOR SUB 1: ENG SUB 2: MATH
+	int stu_num[SUB_NUM][STU_NUM] = { 0, };
 	char name[SUB_NUM][STU_NUM] = { 0, };
 	char major[SUB_NUM][STU_NUM] = { 0, };
 
@@ -41,6 +46,7 @@ int main() {
 	for (int i = 0; i < STU_NUM; i++) {
 		char tmp_name = '0';
 		char tmp_major = '0';
+		int tmp_num = 0;
 
 		printf("%d번째 이름 입력 해주세요 : ", i + 1);
 		if (!scanf("%c", &tmp_name)) exit(1);
@@ -50,9 +56,14 @@ int main() {
 		if (!scanf("%c", &tmp_major)) exit(1);
 		if (!getchar()) exit(1);
 
+		printf("%d번째 학번 입력 해주세요 : ", i + 1);
+		if (!scanf("%d", &tmp_num)) exit(1);
+		if (!getchar()) exit(1);
+
 		for (int j = 0; j < SUB_NUM; j++) {
 			name[j][i] = tmp_name; // SUB 0 : KOR SUB 1: ENG SUB 2: MATH
 			major[j][i] = tmp_major;// SUB 0 : KOR SUB 1: ENG SUB 2: MATH
+			stu_num[j][i] = tmp_num;
 
 			printf("%d번째 국영수 성적 입력 해주세요 : ", i + 1);
 			if (!scanf("%d", &score[j][i]) || score[j][i] > 100) exit(1);
@@ -61,7 +72,7 @@ int main() {
 	}
 
 	for (int i = 0; i < SUB_NUM; i++) {
-		sort(score[i], name[i], major[i], STU_NUM);
+		sort(score[i], stu_num[i], name[i], major[i], STU_NUM);
 	}
 
 
@@ -80,7 +91,7 @@ int main() {
 		int tmp_rank = 0;
 		for (int i = 0; i < STU_NUM; i++) {
 			tmp_rank = i + 1;
-			printf("%d위 : %c 학과 %c %d \n", rank, name[j][i], major[j][i], score[j][i]);
+			printf("%d위 : %c 학과 %c 학번 %d %d \n", rank, name[j][i], major[j][i], stu_num[j][i], score[j][i]);
 			if ((i + 1 < STU_NUM) && (score[j][i] == score[j][i + 1])) {
 				continue;
 			}
