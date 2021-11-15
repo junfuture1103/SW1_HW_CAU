cc00e34b50372a2f1aab97111ecfec85bc176000
JUNTHEWORLD <junhak1103@naver.com>
2021-11-15 23:25:34 +0900

    Modify 1&2

--- SW1_HW10_FUNCTION & VARIABLE/HW10_1_20196487이준학.c
+++ SW1_HW10_FUNCTION & VARIABLE/HW10_1_20196487이준학.c
@@ -4,16 +4,22 @@
 #define TRUE 1
 #define FALSE 0
 
-void avg_ascii(int* total_ascii, int* total_length, int tmp_size) {
-	*total_ascii += tmp_size;
-	(*total_length)++;
+void avg_ascii(int* total_ascii, float* total_length, int tmp_size) {
+	if (65 <= tmp_size && tmp_size <= 122) { //대문자 인경우 A == 65 Z == 90
+		*total_ascii += tmp_size;
+		(*total_length)++;
+		if (97 <= tmp && tmp <= 122) {
+			*total_ascii /= tmp_size;
+		}
+	}
+
 	return;
 }
 
 int main() {
 	char tmp;
 	int end_flag = TRUE;
-	int total_ascii = 0;
+	float total_ascii = 0;
 	int total_length = 0;
 	char word[MAX_SIZE] = { 0, };
 
@@ -29,7 +35,7 @@ int main() {
 		}
 	}
 
-	printf("%s 단어길이 : %d ASCII코드 평균 값 : %.2f", word, total_length, (float)total_ascii / total_length);
+	printf("%s 단어길이 : %d ASCII코드 평균 값 : %.2f", word, total_length, total_ascii);
 
 	return 0;
 }
--- SW1_HW10_FUNCTION & VARIABLE/HW10_2_20196487이준학.c
+++ SW1_HW10_FUNCTION & VARIABLE/HW10_2_20196487이준학.c
@@ -4,51 +4,63 @@
 #define TRUE 1
 #define FALSE 0
 
-int recu_fib(int n) {
-	if (n < 3) {
-		return 1;
-	}
-	return recu_fib(n - 2) + recu_fib(n - 1);
-}
-
-void recu_nodu_fib(int n, int* total) {
-	if (n < 3) {
-		(*total) = 1;
+void recu_fib(int n, int list[]) {
+	if (n == 1 || n == 2) {
+		list[n - 1] = 1;
 		return;
 	}
-	return recu_fib(n - 2) + recu_fib(n - 1);
+	recu_fib(n - 1, list);
+	recu_fib(n - 2, list);
+	list[n - 1] = list[n - 2] + list[n - 3];
+	return;
 }
 
-int iter_fib(int n) {
+/*
+void recu_nodu_fib(int n, int list[]) {
 	if (n < 3) {
 		return 1;
 	}
 
-	int a1 = 1;
-	int a2 = 1;
-	int tmp;
+	return recu_fib(n - 2) + recu_fib(n - 1);
+}
+*/
 
-	for (int i = 0; i < n - 2; i++) {
-		tmp = a1 + a2;
-		a1 = a2;
-		a2 = tmp;
+void iter_fib(int n, int list[]) {
+	if (n < 3) {
+		printf("올바른 수를 입력해주세요 n>=3");
+		return;
 	}
 
-	return tmp;
+	list[0] = list[1] = 1;
+
+	for (int i = 2; i < n; i++) {
+		list[i] = list[i - 1] + list[i - 2];
+	}
+	return;
 }
 
 int main() {
-	int test = 0;
-	int n = 5;
+	int n = 0;
 	int total = 0;
+	int list[MAX_SIZE] = { 0, };
 
-	test = iter_fib(n);
-	printf("ITER : %d\n", test);
+	printf("피보나치 항 개수를 입력해주세요 : ");
+	if (!scanf("%d", &n) || n > 20) exit(1);
 
-	test = recu_fib(n);
-	printf("RECU : %d\n", test);
+	iter_fib(n, list);
+	printf("ITER : ");
 
-	test = recu_nodu_fib(n, &total);
-	printf("RECU (non duplicate) : %d", test);
+	for (int i = 0; i < n; i++) {
+		printf("%d ", list[i]);
+		list[i] = 0;
+	}
+
+	recu_fib(n, list);
+	printf("\nREC : ");
+
+	for (int i = 0; i < n; i++) {
+		printf("%d ", list[i]);
+	}
+
 	return 0;
 }
