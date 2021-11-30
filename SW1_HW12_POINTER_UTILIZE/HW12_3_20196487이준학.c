6c2437ee04069b5ffbaf62e958c118f1622104ed
JUNTHEWORLD <junhak1103@naver.com>
2021-11-30 14:29:22 +0900

    Update HW12_3_20196487이준학.c

--- SW1_HW12_POINTER_UTILIZE/HW12_3_20196487이준학.c
+++ SW1_HW12_POINTER_UTILIZE/HW12_3_20196487이준학.c
@@ -5,8 +5,14 @@
 #define GROUP_NUM 4
 #define STU_NUM 10
 
+#define IS 0
+#define CS 1
+#define ECE 2
+#define MA 3
+
 void sort(char list[][NAME_SIZE], int index) {
-	int min_index, temp;
+	int min_index;
+	char temp[NAME_SIZE];
 
 	for (int i = 0; i < index - 1; i++) {
 		min_index = i;
@@ -15,9 +21,9 @@ void sort(char list[][NAME_SIZE], int index) {
 				min_index = j;
 			}
 		}
-		temp = i;
-		i = min_index;
-		min_index = temp;
+		strcpy(temp, list[i]);
+		strcpy(list[i], list[min_index]);
+		strcpy(list[min_index], temp);
 	}
 }
 
@@ -26,12 +32,11 @@ int main()
 	char major[STU_NUM][MJ_SIZE];
 
 	int index_list[STU_NUM] = { 0,1,2,3,4,5,6,7,8,9 };
-	int index[GROUP_NUM] = { 0, };
 
-	char IS_GROUP[STU_NUM][NAME_SIZE];
-	char CS_GROUP[STU_NUM][NAME_SIZE];
-	char ECE_GROUP[STU_NUM][NAME_SIZE];
-	char MA_GROUP[STU_NUM][NAME_SIZE];
+	int index[GROUP_NUM] = { 0, };
+	int start_index[GROUP_NUM] = { 0, };
+
+	char GROUP[GROUP_NUM][STU_NUM][NAME_SIZE];
 
 	for (int i = 0; i < STU_NUM; i++) {
 		printf("%d번째 학생 학과 : ", i + 1);
@@ -39,43 +44,94 @@ int main()
 
 		if (strcmp(major[i], "IS") == 0) {
 			printf("%d번째 학생 이름 : ", i + 1);
-			if (!scanf("%s", IS_GROUP[index[0]])) exit(1);
-			index[0]++;
+			if (!scanf("%s", GROUP[IS][index[IS]])) exit(1);
+			index[IS]++;
 		}
 		else if (strcmp(major[i], "CS") == 0) {
 			printf("%d번째 학생 이름 : ", i + 1);
-			if (!scanf("%s", CS_GROUP[index[1]])) exit(1);
-			index[1]++;
+			if (!scanf("%s", GROUP[CS][index[CS]])) exit(1);
+			index[CS]++;
 		}
 		else if (strcmp(major[i], "ECE") == 0) {
 			printf("%d번째 학생 이름 : ", i + 1);
-			if (!scanf("%s", ECE_GROUP[index[2]])) exit(1);
-			index[2]++;
+			if (!scanf("%s", GROUP[ECE][index[ECE]])) exit(1);
+			index[ECE]++;
 		}
 		else if (strcmp(major[i], "MA") == 0) {
 			printf("%d번째 학생 이름 : ", i + 1);
-			if (!scanf("%s", MA_GROUP[index[3]])) exit(1);
-			index[3]++;
+			if (!scanf("%s", GROUP[MA][index[MA]])) exit(1);
+			index[MA]++;
 		}
 		else {
 			exit(1);
 		}
 	}
 
-	sort(IS_GROUP, index[0]);
-	sort(CS_GROUP, index[1]);
-	sort(ECE_GROUP, index[2]);
-	sort(MA_GROUP, index[3]);
+	for (int i = 0; i < GROUP_NUM; i++) {
+		sort(GROUP[i], index[i]);
+	}
 
-	int min_group = 0;
+	//정렬 후 그룹 별 이름 출력
+	printf("\n====== 그룹 별 정렬 후 이름 출력 ======\n\n");
+	for (int i = 0; i < GROUP_NUM; i++) {
+		printf("%d번째 그룹\n", i);
+		for (int j = 0; j < index[i]; j++) {
+			printf("%s", GROUP[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 
-	int IS_index = 0;
-	int CS_index = 0;
-	int ECE_index = 0;
-	int MA_index = 0;
+	char min[NAME_SIZE];
+	char find = 1;
+	char result_group[STU_NUM][NAME_SIZE] = { 0, };
+	int result_group_mj[STU_NUM] = { 0, };
+
+	for (int j = 0; j < STU_NUM; j++) {
+		//초기 min 값 찾기
+		for (int i = 0; i < GROUP_NUM; i++) {
+			if (start_index[i] < index[i]) {
+				strcpy(min, GROUP[i][start_index[i]]);
+				result_group_mj[j] = i;
+				find = 1;
+				break;
+			}
+		}
+
+		//못찾은 경우는 start_index가 모두 인덱스보다 큰 경우 (새로운 배열 생성이 완료)
+		if (!find) {
+			break;
+		}
+
+		for (int i = 0; i < GROUP_NUM; i++) {
+			if (strcmp(min, GROUP[i][start_index[i]]) == 1) {
+				strcpy(min, GROUP[i][start_index[i]]);
+				result_group_mj[j] = i;
+			}
+		}
+		start_index[result_group_mj[j]]++;
+		strcpy(result_group[j], min);
+	}
 
 	for (int i = 0; i < STU_NUM; i++) {
-		if (strcmp(IS_GROUP[IS_index], min_group) == 0) {
+		char tmp_mj[MJ_SIZE + 1] = { 0, };
+		switch (result_group_mj[i]) {
+		case 0:
+			strcpy(tmp_mj, "IS");
+			break;
+		case 1:
+			strcpy(tmp_mj, "CS");
+			break;
+		case 2:
+			strcpy(tmp_mj, "ECE");
+			break;
+		case 3:
+			strcpy(tmp_mj, "MA");
+			break;
+		}
+
+		printf("%s", result_group[i]);
+		printf("%s ", tmp_mj);
 	}
 
 	return 0;
